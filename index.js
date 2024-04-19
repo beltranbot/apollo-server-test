198e399e0b8c4535a7dfbf67eb92fa99f500ba29
beltranbot <beltranbot@gmail.com>
2024-04-19 11:39:18 -0500

    updateGame mutation

--- index.js
+++ index.js
@@ -55,6 +55,15 @@ const resolvers = {
       db.games.push(game);
       return game;
     },
+    updateGame(_, args) {
+      db.games = db.games.map(game => {
+        if (game.id === args.id) {
+          return {...game, ...args.edits};
+        }
+        return game;
+      });
+      return db.games.find(game => game.id === args.id);
+    },
     deleteGame(_, args) {
       db.games = db.games.filter(game => args.id !== game.id);
       return db.games;
--- schema.js
+++ schema.js
@@ -33,13 +33,18 @@ export const typeDefs = `#graphql
   type Mutation {
     addGame(game: AddGameInput): Game
     deleteGame(id: ID!): [Game]
+    updateGame(id: ID!, edits: EditGameInput!): Game
   }
 
   input AddGameInput {
     title: String!
     platform: [String!]!
   }
-`
 
+  input EditGameInput {
+    title: String
+    platform: [String!]
+  }
+`
 
 // int, float, string, boolean, ID
